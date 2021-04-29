e92ff220ad1c2953c47f15e96fa8cdb89d354818
brecert <11599528+Brecert@users.noreply.github.com>
2021-04-29 16:26:14 -0400

    improve tests to test all valid emojis

--- emoji_test.ts
+++ emoji_test.ts
@@ -1,5 +1,5 @@
 import { EMOJI_KEYCAP_SEQUENCE, EMOJI_SEQUENCE } from "./emoji.ts";
-import { assertEquals } from "https://deno.land/std@0.91.0/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.94.0/testing/asserts.ts";
 
 Deno.test("EMOJI_KEYCAP_SEQUENCE", () => {
   assertEquals(EMOJI_KEYCAP_SEQUENCE.test("Hello World!"), false);
@@ -12,4 +12,16 @@ Deno.test("EMOJI_SEQUENCE", () => {
   assertEquals(EMOJI_SEQUENCE.test("1"), false);
   assertEquals(EMOJI_SEQUENCE.test("1️⃣"), true);
   assertEquals(EMOJI_SEQUENCE.test("🏳️‍🌈"), true);
+});
+
+const EMOJI_DATA_URL =
+  "https://raw.githubusercontent.com/milesj/emojibase/emojibase@5.1.1/packages/data/en/data.raw.json";
+
+Deno.test("EMOJI_SEQUENCE", async () => {
+  const data = await fetch(EMOJI_DATA_URL).then((r) => r.json());
+  for (let i = 0; i < data; i++) {
+    const emoji: string = data[i];
+    const match = emoji.match(EMOJI_SEQUENCE);
+    assertEquals(match?.[0], emoji);
+  }
 });
